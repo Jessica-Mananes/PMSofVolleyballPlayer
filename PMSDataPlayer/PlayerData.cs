1fa6d1dbe6d7d277bbfa0d09a362d484cea97dcf
Jessica Mananes <jessicamananes@gmail.com>
2025-04-10 13:56:55 +0800

    Updated

---
+++ PMSDataPlayer/PlayerData.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using PMSDataPlayer;
+
+namespace PMSDataPlayer
+{
+    public class PlayerData
+    {
+
+        private readonly List<Player> players = new();
+
+        public void AddPlayer(Player player)
+        {
+            players.Add(player);
+        }
+
+        public bool UpdatePlayer(int index, string name, int age, string position)
+        {
+            if (index >= 0 && index < players.Count)
+            {
+                players[index].Name = name;
+                players[index].Age = age;
+                players[index].Position = position;
+                return true;
+            }
+            return false;
+        }
+
+        public bool DeletePlayer(int index)
+        {
+            if (index >= 0 && index < players.Count)
+            {
+                players.RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return players;
+        }
+
+        public int GetPlayerCount()
+        {
+            return players.Count;
+        }
+
+        public Player GetPlayerByIndex(int index)
+        {
+            if (index >= 0 && index < players.Count)
+                return players[index];
+            return null;
+        }
+    }
+}
