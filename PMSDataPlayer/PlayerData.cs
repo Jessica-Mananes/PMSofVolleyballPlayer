707e2add25d044d88c43827eb5bf9ade342a4ef9
Jessica Mananes <jessicamananes@gmail.com>
2025-05-22 15:03:32 +0800

    Update PlayerData.cs

--- PMSDataPlayer/PlayerData.cs
+++ PMSDataPlayer/PlayerData.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PMSDataPlayer
 {
-    class PlayerData
+    public class PlayerData
     {
+        private readonly IPlayerDataSource playerData;
+
+        public PlayerData()
+        {
+            playerData = new JsonFilePlayerData();
+            // playerData = new TextFilePlayerData();
+            // playerData = new InMemoryPlayerData();
+        }
+
+        public List<Player> GetAllPlayers()
+        {
+            return playerData.GetAllPlayers();
+        }
+
+        public bool AddPlayer(Player player)
+        {
+            return playerData.AddPlayer(player);
+        }
+
+        public bool EditPlayer(Player updatedPlayer)
+        {
+            var existingPlayer = playerData.GetPlayerByName(updatedPlayer.Name);
+            if (existingPlayer == null)
+                return false;
+
+            existingPlayer.Age = updatedPlayer.Age;
+            existingPlayer.Position = updatedPlayer.Position;
+
+            return playerData.UpdatePlayer(existingPlayer);
+        }
+
+        public bool DeletePlayer(string playerName)
+        {
+            return playerData.DeletePlayer(playerName);
+        }
+
+        public Player GetPlayerByName(string playerName)
+        {
+            return playerData.GetPlayerByName(playerName);
+        }
+
+        public List<Player> SearchPlayersByName(string searchTerm)
+        {
+            return playerData.SearchPlayersByName(searchTerm);
+        }
     }
 }
