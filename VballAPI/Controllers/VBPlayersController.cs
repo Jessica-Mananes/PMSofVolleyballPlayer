f401b41c319e6cd701a8acb8870592ad174681a2
Jessica Mananes <jessicamananes@gmail.com>
2025-06-23 23:44:13 +0800

    IMPLEMENTED API

---
+++ VballAPI/Controllers/VBPlayersController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PlayerCommon;
+using PMSDataPlayer;
+using VballPMS;
+
+namespace VballAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VBPlayersController : ControllerBase
+    {
+        PlayerService playerService = new PlayerService(new InMemoryPlayerData());
+
+        [HttpGet]
+        public IEnumerable<Player> GetAllPlayers()
+        {
+            return playerService.GetAllPlayers();
+        }
+
+        [HttpPost]
+        public bool AddPlayer(Player request)
+        {
+            return playerService.AddPlayer(request);
+        }
+
+        [HttpPatch("update")]
+        public bool UpdatePlayer(string name, Player updatedPlayer)
+        {
+            return playerService.UpdatePlayer(name, updatedPlayer);
+        }
+
+        [HttpDelete]
+        public bool DeletePlayer(string name)
+        {
+            return playerService.DeletePlayer(name);
+        }
+
+        [HttpGet("search")]
+        public IEnumerable<Player> SearchPlayersByName(string keyword)
+        {
+            return playerService.SearchPlayersByName(keyword);
+        }
+    }
+}
