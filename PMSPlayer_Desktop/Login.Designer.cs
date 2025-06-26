398dfb3fc75c77328d7f9f94061280435ebf9f5e
Jessica Mananes <jessicamananes@gmail.com>
2025-06-26 14:16:37 +0800

    .

--- PMSPlayer_Desktop/Login.Designer.cs
+++ PMSPlayer_Desktop/Login.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace VballPlayer_Desktop
+﻿namespace PMSPlayer_Desktop
 {
     partial class Login
     {
@@ -28,96 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.tbxPassword = new System.Windows.Forms.TextBox();
-            this.tbxUsername = new System.Windows.Forms.TextBox();
-            this.lblPassword = new System.Windows.Forms.Label();
-            this.lblUserName = new System.Windows.Forms.Label();
-            this.lblSystemName = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            btnLogin = new Button();
+            tbxPassword = new TextBox();
+            tbxUsername = new TextBox();
+            lblPassword = new Label();
+            lblUserName = new Label();
+            lblSystemName = new Label();
+            SuspendLayout();
             // 
             // btnLogin
             // 
-            this.btnLogin.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.btnLogin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btnLogin.Font = new System.Drawing.Font("Microsoft Himalaya", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.btnLogin.Location = new System.Drawing.Point(428, 552);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(172, 43);
-            this.btnLogin.TabIndex = 11;
-            this.btnLogin.Text = "Login";
-            this.btnLogin.UseVisualStyleBackColor = false;
+            btnLogin.BackColor = Color.CornflowerBlue;
+            btnLogin.BackgroundImageLayout = ImageLayout.Zoom;
+            btnLogin.Cursor = Cursors.Hand;
+            btnLogin.FlatStyle = FlatStyle.Popup;
+            btnLogin.Font = new Font("Microsoft Himalaya", 28.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLogin.ForeColor = SystemColors.ControlText;
+            btnLogin.Location = new Point(424, 651);
+            btnLogin.Margin = new Padding(3, 4, 3, 4);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(172, 54);
+            btnLogin.TabIndex = 11;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
             // 
             // tbxPassword
             // 
-            this.tbxPassword.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxPassword.Location = new System.Drawing.Point(301, 398);
-            this.tbxPassword.Name = "tbxPassword";
-            this.tbxPassword.Size = new System.Drawing.Size(311, 40);
-            this.tbxPassword.TabIndex = 10;
+            tbxPassword.Font = new Font("Impact", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbxPassword.Location = new Point(301, 498);
+            tbxPassword.Margin = new Padding(3, 4, 3, 4);
+            tbxPassword.Name = "tbxPassword";
+            tbxPassword.Size = new Size(311, 40);
+            tbxPassword.TabIndex = 10;
             // 
             // tbxUsername
             // 
-            this.tbxUsername.Font = new System.Drawing.Font("Impact", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbxUsername.Location = new System.Drawing.Point(301, 326);
-            this.tbxUsername.Name = "tbxUsername";
-            this.tbxUsername.Size = new System.Drawing.Size(311, 40);
-            this.tbxUsername.TabIndex = 9;
+            tbxUsername.Font = new Font("Impact", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            tbxUsername.Location = new Point(301, 408);
+            tbxUsername.Margin = new Padding(3, 4, 3, 4);
+            tbxUsername.Name = "tbxUsername";
+            tbxUsername.Size = new Size(311, 40);
+            tbxUsername.TabIndex = 9;
             // 
             // lblPassword
             // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPassword.Location = new System.Drawing.Point(139, 403);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(134, 35);
-            this.lblPassword.TabIndex = 8;
-            this.lblPassword.Text = "password:";
+            lblPassword.AutoSize = true;
+            lblPassword.Font = new Font("Impact", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblPassword.Location = new Point(139, 504);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(134, 35);
+            lblPassword.TabIndex = 8;
+            lblPassword.Text = "password:";
             // 
             // lblUserName
             // 
-            this.lblUserName.AutoSize = true;
-            this.lblUserName.Font = new System.Drawing.Font("Impact", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUserName.Location = new System.Drawing.Point(135, 326);
-            this.lblUserName.Name = "lblUserName";
-            this.lblUserName.Size = new System.Drawing.Size(138, 35);
-            this.lblUserName.TabIndex = 7;
-            this.lblUserName.Text = "username:";
+            lblUserName.AutoSize = true;
+            lblUserName.Font = new Font("Impact", 16.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblUserName.Location = new Point(135, 408);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(138, 35);
+            lblUserName.TabIndex = 7;
+            lblUserName.Text = "username:";
             // 
             // lblSystemName
             // 
-            this.lblSystemName.AutoSize = true;
-            this.lblSystemName.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.lblSystemName.Font = new System.Drawing.Font("Britannic Bold", 28.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSystemName.ForeColor = System.Drawing.Color.Navy;
-            this.lblSystemName.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
-            this.lblSystemName.Location = new System.Drawing.Point(53, 111);
-            this.lblSystemName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblSystemName.Name = "lblSystemName";
-            this.lblSystemName.Size = new System.Drawing.Size(611, 104);
-            this.lblSystemName.TabIndex = 6;
-            this.lblSystemName.Text = "          Volleyball Player\r\n Profile Management System";
-            this.lblSystemName.Click += new System.EventHandler(this.lblSystemName_Click);
+            lblSystemName.AutoSize = true;
+            lblSystemName.BackColor = Color.CornflowerBlue;
+            lblSystemName.Font = new Font("Britannic Bold", 28.2F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblSystemName.ForeColor = Color.Navy;
+            lblSystemName.ImageAlign = ContentAlignment.BottomCenter;
+            lblSystemName.Location = new Point(53, 139);
+            lblSystemName.Margin = new Padding(4, 0, 4, 0);
+            lblSystemName.Name = "lblSystemName";
+            lblSystemName.Size = new Size(611, 104);
+            lblSystemName.TabIndex = 6;
+            lblSystemName.Text = "          Volleyball Player\r\n Profile Management System";
+            lblSystemName.Click += lblSystemName_Click;
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::VballPlayer_Desktop.Properties.Resources.bgLogin;
-            this.ClientSize = new System.Drawing.Size(696, 839);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.tbxPassword);
-            this.Controls.Add(this.tbxUsername);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.lblUserName);
-            this.Controls.Add(this.lblSystemName);
-            this.Name = "Login";
-            this.Text = "Form1";
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = PMSPlayer_Desktop.Properties.Resources.bgLogin;
+            ClientSize = new Size(696, 1049);
+            Controls.Add(btnLogin);
+            Controls.Add(tbxPassword);
+            Controls.Add(tbxUsername);
+            Controls.Add(lblPassword);
+            Controls.Add(lblUserName);
+            Controls.Add(lblSystemName);
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "Login";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
